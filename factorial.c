b802be95727c4bd0c7f46b39cc0a854c4fbc4055
Parv shah <parvs247@gmail.com>
2026-03-17 00:17:22 +0530

    add new files

---
+++ factorial.c
@@ -0,0 +1,12 @@
+//find factorial of a given number
+#include<stdio.h>
+int main(){
+    int n, fact=1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+    printf("Factorial of %d is %d", n, fact);
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+   int a,b;
+
+   printf("Enter the number a and b ");
+    scanf("%d %d",&a,&b);
+   printf("Sum is  %d",a+b);
+   return 0;
+
+}
